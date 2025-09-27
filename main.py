b3a07e1914e87304352de13218a05aa9175a77d0
kondratyevan <kondratyevan@ozon.ru>
2025-09-27 21:31:15 +0300

    [hw-1] sum of 3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calc_sum(a: int, b: int) -> int:
-    return a + b
+def calc_sum(a: int, b: int, c: int) -> int:
+    return a + b + c
 
 if __name__ == "__main__":
-    x, y = map(int, input().split())
-    print(calc_sum(x, y))
+    x, y, z = map(int, input().split())
+    print(calc_sum(x, y, z))
